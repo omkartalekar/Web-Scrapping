2eacc461f69f4d6509cb4b7606ed36895639daf9
omkar <omkartalekar1112@gmail.com>
2021-10-22 09:57:28 +0530

    first commit

--- WEB SCRAPPING - JASBIR BHAIYA/ESPNCRICINFO PROJECT/request.js
+++
@@ -1,9 +0,0 @@
-const request = require("request");
-request("http://www.worldometers.info/coronavirus/",cb);
-
-function cb(error, response,html) {
-
-  console.error('error:', error); 
-  console.log('statusCode:', response && response.statusCode);
-  console.log('html:', html); 
-};
